d816de898fca7a8710cef4d36e8e29e85d461d13
Ankeet Maini <ankeet.maini@gmail.com>
2020-02-16 13:14:43 +0530

    Finally fix babel config by downgrading next-transpile-modules

--- next.config.js
+++ next.config.js
@@ -1,21 +1,44 @@
+const webpack = require('webpack');
 const withImages = require('next-images');
+const withTM = require('next-transpile-modules');
 
-module.exports = withImages({
-  webpack: (config, {defaultLoaders}) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      // Transform all direct `react-native` imports to `react-native-web`
-      'react-native$': 'react-native-web',
-    };
-
-    // resolve web files first!
-    config.resolve.extensions = [
-      '.web.js',
-      '.web.ts',
-      '.web.tsx',
-      ...config.resolve.extensions,
-    ];
-
-    return config;
-  },
-});
+module.exports = withImages(
+  withTM({
+    transpileModules: [
+      'react-native',
+      'react-navigation',
+      'react-native-gesture-handler',
+      'react-native-reanimated',
+      'react-native-screens',
+    ],
+    webpack: config => {
+      return {
+        ...config,
+        plugins: [
+          ...config.plugins,
+          new webpack.DefinePlugin({
+            __DEV__: JSON.stringify(process.env.NODE_ENV),
+          }),
+        ],
+        resolve: {
+          ...config.resolve,
+          extensions: [
+            '.web.ts',
+            '.web.tsx',
+            '.ts',
+            '.tsx',
+            '.web.js',
+            '.web.jsx',
+            '.js',
+            '.jsx',
+            ...config.resolve.extensions,
+          ],
+          alias: {
+            ...config.resolve.alias,
+            'react-native$': 'react-native-web',
+          },
+        },
+      };
+    },
+  }),
+);
